ba502f500e24ce25a8c4977aa3c61da07c760930
PerceusAdalian <88364710+PerceusAdalian@users.noreply.github.com>
2026-08-03 13:49:25 -0500

    Rewrite

--- index.js
+++ index.js
@@ -21,3 +21,36 @@ async function logAPIData()
 }
 
 logAPIData();
+
+function openMenu() {
+    document.body.classList +=  ' menu--open';
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open');
+}
+
+/*
+
+<div class="book">
+    <figure class="book__card--wrapper">
+        <img src="./assets/crack the coding interview.png" alt="" class="book__card">
+    </figure>
+    <div class="book__description">
+        <a class="book__title">
+            Crack the Coding Interview
+        </a>
+        <div class="book__ratings">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+        <div class="book__price">
+            <span class="book__price--normal">$59.95</span> $14.95
+        </div>
+    </div>
+</div>
+
+*/
