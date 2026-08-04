534108309d17a542b2f5ef71bd9e6b31aa9b0ca5
Percy <davidmwilly@gmail.com>
2026-08-04 17:34:59 -0500

    Additions

--- index.js
+++ index.js
@@ -1,56 +1,127 @@
-async function getAPIData() 
-{
+let movies;
+
+async function searchMovies(query) {
     const response = await fetch('http://www.omdbapi.com/?' + new URLSearchParams({
         apikey: 'aa8d4a8a',
-        s: 'Inception'
+        s: query
     }));
+    const data = await response.json();
 
-    if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
+    if (data.Response === 'False') {
+        console.error(data.Error);
+        return [];
     }
 
-    const data = await response.json();
-    console.log(data);
-    return data;
+    const fullMovies = await Promise.all(
+        data.Search.map(async (movie) => {
+            const detailRes = await fetch('http://www.omdbapi.com/?' + new URLSearchParams({
+                apikey: 'aa8d4a8a',
+                i: movie.imdbID
+            }));
+            return detailRes.json();
+        })
+    );
+
+    return fullMovies;
 }
 
-async function logAPIData()
+function hasValidPoster(poster) {
+    return poster && poster !== 'N/A';
+}
+
+function buildPlaceholderHTML(title) {
+    return `<div class="movie__card movie__card--placeholder">
+        <i class="fa-solid fa-film"></i>
+        <span>${title}</span>
+    </div>`;
+}
+
+async function renderMovies(data, filter) 
 {
-    const data = await getAPIData();
-    return data;
+    const moviesWrapper = document.querySelector('.movies__rendered');
+
+    const moviesHTML = data.map(data => {
+        const posterHTML = hasValidPoster(data.Poster)
+            ? `<img src="${data.Poster}" alt="${data.Title}" class="movie__card" data-title="${data.Title}">`
+            : buildPlaceholderHTML(data.Title);
+
+        return `<div class="movie" data-id="${data.imdbID}">
+            <figure class="movie__card--wrapper">
+                ${posterHTML}
+            </figure>
+            <div class="movie__description">
+                <a href="${data.imdbID}" class="movie__title">
+                    ${data.Title} (${data.Year})
+                </a>
+                <div class="movie__ratings">
+                    <i class="fa-solid fa-star"></i> - ${data.imdbRating}
+                </div>
+                <p><span class="text--red" style="font-weight:bold;">Genre</span>: ${data.Genre}</p>
+                <p><span class="text--red" style="font-weight:bold;">Plot</span>: ${data.Plot}</p>
+            </div>
+        </div>`;
+    }).join('');
+
+    moviesWrapper.innerHTML = moviesHTML;
+
+    // Catch images that had a valid-looking URL but failed to actually load
+    moviesWrapper.querySelectorAll('.movie__card').forEach(img => {
+        if (img.tagName !== 'IMG') return; // skip ones already rendered as placeholders
+
+        img.addEventListener('error', () => {
+            const title = img.dataset.title;
+            img.outerHTML = buildPlaceholderHTML(title);
+        }, { once: true });
+    });
 }
 
-logAPIData();
-
-function openMenu() {
-    document.body.classList +=  ' menu--open';
+function sortMovies(data, sortType) 
+{
+    const sorted = [...data];
+    switch (sortType) 
+    {
+        case 'RATING_HIGH_TO_LOW':
+            sorted.sort((a, b) => (parseFloat(b.imdbRating) || 0) - (parseFloat(a.imdbRating) || 0));
+            break;
+        case 'RATING_LOW_TO_HIGH':
+            sorted.sort((a, b) => (parseFloat(a.imdbRating) || 0) - (parseFloat(b.imdbRating) || 0));
+            break;
+        case 'TITLE_A_TO_Z':
+            sorted.sort((a, b) => a.Title.localeCompare(b.Title));
+            break;
+        case 'TITLE_Z_TO_A':
+            sorted.sort((a, b) => b.Title.localeCompare(a.Title));
+            break;
+        case 'YEAR_NEWEST':
+            sorted.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+            break;
+        case 'YEAR_OLDEST':
+            sorted.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
+            break;
+    }
+    return sorted;
 }
 
-function closeMenu() {
+function filterByGenre(data, genre) 
+{
+    if (!genre) return data;
+    return data.filter(movie => movie.Genre.includes(genre));
+}
+
+async function initHomePage(query, filter) 
+{
+    const movies = await searchMovies(query);
+    renderMovies(movies, filter);
+}
+
+initHomePage('Inception');
+
+function openMenu() 
+{
+    document.body.classList.add('menu--open');
+}
+
+function closeMenu() 
+{
     document.body.classList.remove('menu--open');
 }
-
-/*
-
-<div class="book">
-    <figure class="book__card--wrapper">
-        <img src="./assets/crack the coding interview.png" alt="" class="book__card">
-    </figure>
-    <div class="book__description">
-        <a class="book__title">
-            Crack the Coding Interview
-        </a>
-        <div class="book__ratings">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star-half-alt"></i>
-        </div>
-        <div class="book__price">
-            <span class="book__price--normal">$59.95</span> $14.95
-        </div>
-    </div>
-</div>
-
-*/
